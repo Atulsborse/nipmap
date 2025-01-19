00a07e335d000ab897d3c3cd4b13b1a292386ad0
Atulisboss <Atulborse2000@gmail.com>
2025-01-19 11:36:34 +0530

    Initial commit

---
+++ backend/models/productModel.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const Category = require('./categoryModel');
+
+const Product = sequelize.define('Product', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    references: { model: Category, key: 'id' },
+  },
+}, { timestamps: true });
+
+Product.belongsTo(Category, { foreignKey: 'categoryId' });
+
+module.exports = Product;
